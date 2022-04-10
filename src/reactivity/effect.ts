1f3657136aedad5907e71c0d813d7cedec4da9cf
wangweiguo2013 <wangwg1994@163.com>
2022-04-10 14:32:49 +0800

    feat:effect scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,10 +1,12 @@
-class ReactiveEffect{
+class ReactiveEffect {
     private _fn: any
-    constructor(fn){
+    public scheduler: any
+    constructor(fn, scheduler?) {
         this._fn = fn
+        this.scheduler = scheduler
     }
 
-    run(){
+    run() {
         // 全局一次只有一个正在执行的effect(vue2中的watcher)
         activeEffect = this
         return this._fn()
@@ -14,35 +16,39 @@ class ReactiveEffect{
 const targetMap = new Map()
 let activeEffect
 
-export const trigger = function(target, key){
+export const trigger = function (target, key) {
     // 取出该key的依赖
     const depMap = targetMap.get(target)
-  
+
     const dep = depMap.get(key)
     // 执行依赖
     dep.forEach((_effect) => {
-        _effect.run()
+        if (_effect.scheduler) {
+            _effect.scheduler()
+        } else {
+            _effect.run()
+        }
     })
 }
 
 //收集依赖
-export const track  = function(target, key){
+export const track = function (target, key) {
     // 取出该key的依赖
     let depMap = targetMap.get(target)
-    if(!depMap) {
+    if (!depMap) {
         depMap = new Map()
         targetMap.set(target, depMap)
     }
     let dep = depMap.get(key)
-    if(!dep) {
+    if (!dep) {
         dep = new Set()
         depMap.set(key, dep)
     }
     dep.add(activeEffect)
 }
 
-export const effect = function(fn){
-    const _effect = new ReactiveEffect(fn)
+export const effect = function (fn, options = {}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler)
     _effect.run()
 
     return _effect.run.bind(_effect)
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -17,7 +17,7 @@ describe('effect', () => {
         expect(nextAge).toBe(12)
     })
 
-    it('advanced effect', () => {
+    it('effect should return a runner', () => {
         let foo = 10;
         const runner = effect(() => {
             foo++
@@ -28,4 +28,35 @@ describe('effect', () => {
         expect(foo).toBe(12)
         expect(res).toBe('foo')
     })
+
+    it('scheduler', () => {
+        // effect第一次会执行传入的第一个参数 fn
+        // 当响应式对象更新的时候，不会执行fn，而是会执行scheduler
+        // 当执行runner的时候，会再次执行fn
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner
+        })
+        const obj = reactive({ foo: 1 })
+        const runner = effect(
+            () => {
+                dummy = obj.foo
+            },
+            { scheduler }
+        )
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1)
+        // should be called on first trigger
+        obj.foo++
+        expect(scheduler).toHaveBeenCalledTimes(1)
+        // should not run yet
+        expect(dummy).toBe(1)
+        // manually run
+        run()
+        // should have run 
+        expect(dummy).toBe(2)
+
+    })
+
 })
